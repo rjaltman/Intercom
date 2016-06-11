a948f0752d6513d643a732f8b7d1305cca8c980d
Robert J. Altman <rjaltman04@gmail.com>
2016-06-11 02:38:13 -0500

    Setup of Main Function
    Yep. Very basic. It's okay, it will get better...really, it will this
    time.

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void portListen() {
+
+}
+
+int main(int argc, char const *argv[]) {
+
+  std::cout << "Network Messaging Client" << std::endl;
+
+  portListen();
+
+  return 0;
+}
