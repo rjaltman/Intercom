fe48a8f05e9e7b0d6b65715981b9a8179e22be43
Robert J. Altman <rjaltman04@gmail.com>
2016-06-15 01:52:50 -0500

    Set up of portListen()
    I didn't even know this was written, but I might as well commit it while
    I remember it exists (before doing something completely different here).

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 void portListen() {
+  int n;
+  n = 0; // Staying true to ANSI C restrictions, even though this is C++ (welp)
+
+  if (!n) {
+    // Deliberate infinite loop
+    // Ctrl+C is going to happen
+
+
+
+  }
+
 
 }
 
